e78bbbaeae9e8b7e453e11d0acba33b21775f379
Mideli <muzipovanastia@gmail.com>
2023-01-16 11:05:39 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2) {
+    return false;
+  }
+
+  let flag;
+  while (flag != bracketsConfig.length) {
+    flag = 0;
+    for (let i = 0; i < bracketsConfig.length; i++) { //[['(', ')'], ['[', ']'], ['{', '}']]
+      if (str.indexOf(bracketsConfig[i].join('')) != -1) {
+        str = str.replaceAll(bracketsConfig[i].join(''), '');
+      } else flag += 1;
+    }
+  }
+  if (str) return false;
+  return true;
+};
